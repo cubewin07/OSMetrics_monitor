8169bf16a7439e6ae9d29fc9802ec8a114931115
tl493 <tl493@LAB-M318-09>
2026-06-29 13:06:36 +1200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Metrics
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(82, 80);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(541, 193);
+            dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(194, 346);
+            label1.Name = "label1";
+            label1.Size = new Size(28, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Test";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(429, 346);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 2;
+            label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void Custom()
+        {
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += UpdadingMetrics;
+
+            // Configure datagridview
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn { DataPropertyName = "Name", HeaderText = "Name", Name = "Name" });
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn { DataPropertyName = "PID", HeaderText = "PID", Name = "PID" });
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn { DataPropertyName = "MemoryMD", HeaderText = "Memory (MB)", Name = "MemoryMB" });
+            dataGridView1.Columns.Add(new DataGridViewButtonColumn { Name = "Kill", Text = "Kill", UseColumnTextForButtonValue = true, HeaderText = "" });
+
+            dataGridView1.CellContentClick += kill;
+            // Start timer
+            this.timer1.Start();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private Label label2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+using System.Diagnostics;
+using System.ComponentModel;
+
+namespace Metrics
+{
+    public partial class Form1 : Form
+    {
+
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public PerformanceCounter CpuCounter { get; set; } = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public PerformanceCounter RamCounter { get; set; } = new PerformanceCounter("Memory", "Available MBytes");
+
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public int SelectedPID { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            Custom();
+        }
+
+        public void UpdadingMetrics(Object sender, EventArgs e)
+        {
+            Process[] current = Process.GetProcesses();
+
+            var snapshot = current.Select(p => new ProcessSnapshot
+            {
+                Name = p.ProcessName,
+                PID = p.Id,
+                MemoryMD = p.WorkingSet64 / 1024 / 1024,
+                RawProcces = p
+            }).ToList();
+
+            RefreshList(dataGridView1, snapshot);
+            MoveBackToSelectedProcess(SelectedPID);
+
+            label1.Text = CpuCounter.NextValue().ToString();
+            label2.Text = RamCounter.NextValue().ToString();
+
+        }
+        public static void RefreshList<T>(DataGridView target, List<T> list)
+        {
+            target.DataSource = null;
+            target.DataSource = list;
+        }
+
+        public void MoveBackToSelectedProcess(int id)
+        {
+            foreach(DataGridViewRow row in dataGridView1.Rows)
+            {
+                if ((int)row.Cells["PID"].Value == id)
+                {
+                    row.Selected = true;
+                    dataGridView1.CurrentCell = row.Cells[0];
+                    break;
+                }
+            }
+        }
+
+        public void kill(Object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                int pid = (int)dataGridView1.Rows[e.RowIndex].Cells["PID"].Value;
+                if(e.ColumnIndex == dataGridView1?.Columns["Kill"].Index)
+                {
+                    try
+                    {
+                       Process.GetProcessById(pid).Kill();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                    }
+
+                } else
+                {
+                    SelectedPID = pid;
+                }
+            }
+        }
+    }
+
+    public class ProcessSnapshot
+    {
+        public required string Name { get; set; }
+        public int PID { get; set; }
+        public float MemoryMD { get; set; }
+        public required Process RawProcces { get; set; }
+    }
+}
