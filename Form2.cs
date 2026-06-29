e269a4eadff708db66705cfdb16346d01d80b19b
cubewin07 <tanthang071208@gmail.com>
2026-06-30 11:33:14 +1200

    Add update CPU method in Form2

--- Form2.cs
+++ Form2.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,33 @@ namespace Metrics
 {
     public partial class Form2 : Form
     {
+        private DateTime _lastCpuTime;
+        private TimeSpan _lastTotalProcessorTime;
         public Form2()
         {
             InitializeComponent();
+        }
+
+        public Form2(ProcessSnapshot snapshot)
+        {
+            InitializeComponent();
+            UpdateCPU(snapshot.RawProcces);
+        }
+
+        public void UpdateCPU(Process process)
+        {
+            var currentTime = DateTime.UtcNow;
+            var currentProcessorTime = process.TotalProcessorTime;
+
+            double cpuUsed = (currentProcessorTime - _lastTotalProcessorTime).TotalMilliseconds;
+            double elapsed = (currentTime - _lastCpuTime).TotalMilliseconds;
+
+            float cpuPercent = (float)(cpuUsed / (elapsed * Environment.ProcessorCount) * 100);
+
+            _lastCpuTime = currentTime;
+            _lastTotalProcessorTime = currentProcessorTime;
+
+            label1.Text = cpuPercent.ToString();
         }
     }
 }
